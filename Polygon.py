50daf6b7c5b71d8fdd689eeebd361f4ecba1581d
Alonso Bravo Moyano <alobrv@gmail.com>
2021-03-23 00:01:30 +0900

    Added move method

--- Polygon.py
+++ Polygon.py
@@ -1,4 +1,20 @@
+from Point import Point
+from Vector import Vector
+
 class Polygon:
 
     def __init__(self, vertices):
         self.vertices = vertices
+
+    def move(self, vector):
+        for vertex in self.vertices:
+            vertex.x += vector.X()
+            vertex.y += vector.Y()
+
+class main():
+    vertices = [Point(1,1), Point(0,0), Point(0,1)]
+    polygon = Polygon(vertices)
+    polygon.move(Vector(Point(0,0),Point(1,1)))
+    for vertex in polygon.vertices:
+        print(vertex.x)
+        print(vertex.y)
