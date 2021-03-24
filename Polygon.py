3ba91079ba20b84843b2309e61467b2fabe485a9
Alonso Bravo Moyano <alobrv@gmail.com>
2021-03-24 19:20:16 +0900

    Test update

--- Polygon.py
+++ Polygon.py
@@ -35,8 +35,5 @@ class main:
     vertices = [Point(1,1), Point(0,0), Point(0,1)]
     polygon = Polygon(vertices)
     polygon.move(Vector(Point(0,0),Point(1,1)))
-    for vertex in polygon.vertices:
-        print(vertex.x)
-        print(vertex.y)
     print(polygon.centroid().x)
     print(polygon.centroid().y)
